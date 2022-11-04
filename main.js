7188c42f33b37ce55df6dc8e872ea5520b1e928a
VukIG <vuk.ignjatovic05@gmail.com>
2022-11-03 17:28:55 -0800

    alesandre

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ let kurac;
 let points = 0;
 const tileSize = 50;
 let guja = [];
-let xv,yv;
+let v;
 let BRZ=50;
-
+//reakt document.quertselector akgpeqgo
 
 function kraj(){
     let endScreen=document.createElement('div');
@@ -17,13 +17,13 @@ function kraj(){
     </div>
     
     `;
-   document.appendChild(endScreen);     
+   react.appendChild(endScreen);     
     
 }
 
 
 function createApple() {
-    kurac=new p5.Vector(random(0,size/tileSize-1),random(0,size/tileSize)-1);
+    kurac=new p5.Vector(floor(random(0,size/tileSize-1)),floor(random(0,size/tileSize)-1));
     guja.forEach(deoGuje => {
         if(deoGuje.equals(kurac)){
             createApple();
@@ -37,21 +37,25 @@ function createApple() {
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-        xv=-1;
+        v.x=-1;
+        v.y=0;
     } else if (keyCode === RIGHT_ARROW) {
-        xv=1;
+        v.x=1;
+        v.y=0;
     }
     else if (keyCode === DOWN_ARROW) {
-        yv=-1;
+        v.y=1;
+        v.x=0;
     }
     else if (keyCode === UP_ARROW) {
-        yv=1;
+        v.y=-1;
+        v.x=0;
     }
 }
 
 function proveri() {
-    let newx=guja[0].x+xv;
-    let newy=guja[0].y+yv;
+    let newx=guja[0].x+v.x;
+    let newy=guja[0].y+v.y;
     guja.forEach(deoGuje => {
         if(newx==deoGuje.x && newy==deoGuje.y){
             return 0;
@@ -61,31 +65,32 @@ function proveri() {
 }
 
 function snake() {
-    
-    let istina=proveri();
-    if (istina==1){
-        guja[0].set(guja[0].x*xv,guja[0].y*yv);
-        guja[0].x*xv*BRZ;
-        guja[0].y*yv*BRZ;
-        console.log(guja[0])
+    if (guja[0].x*tileSize> width || guja[0].y*tileSize > height || guja[0].x < 0 || guja[0].y < 0) {
+        kraj();
+    }
+    if (guja[0].equals(kurac)) {
+        createApple();
+        points++;
+        guja.push(new p5.Vector(guja.at(-1).x,guja.at(-1).y))
+        for (let i = 1; i < guja.length-1; i++) {
+            if (guja[0].equals(guja[i])) kraj();
+            guja[i].set(guja[i-1]);
+        }
+        
+    }
+    else{
+    for (let i = 1; i < guja.length ; i++) {
+        if (guja[0].equals(guja[i])) kraj();
+        guja[i].set(guja[i-1]);
+    }
+}
+    if (proveri()==1){
+        guja[0].add(v);
     }
     else{
         return;
     }
-    if (guja[0].x == kurac.x && guja[0].y == kurac.y) {
-        createApple();
-        points++;
-        guja.push(new p5.Vector(guja[length-1].x,guja[length-1].y))
-    }
-    if (guja[0].x > height || guja[0].y > width || guja[0].x < 0 || guja[0].y < 0) {
-        kraj();
-        
-    }
-    for (let i = 1; i < guja.length - 1; i++) {
-        if (guja[0].equals(guja[i])) kraj();
-        guja[i].x = guja[i + 1].x;
-        guja[i].y = guja[i + 1].y;
-    }
+    
 }
 
 function crtaj(){
@@ -102,9 +107,14 @@ function crtaj(){
 function setup() {
     createCanvas(size, size);
     guja = [];
-    guja.push(new p5.Vector(0, 0));
+    guja.push(new p5.Vector(4, 4));
+    guja.push(new p5.Vector(3, 4));
+    guja.push(new p5.Vector(2 , 4));
+    guja.push(new p5.Vector(1 , 4));
     ellipseMode(CORNER);
     createApple();
+    noStroke();
+    v=new p5.Vector(0,-1);
 }
 
 
